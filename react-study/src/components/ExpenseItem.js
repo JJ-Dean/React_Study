ebbff239840357ab8744819eb660e59b67042677
Dean <yjj052800@gmail.com>
2022-12-18 17:42:59 +0900

    add custom component

---
+++ react-study/src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>December 18th 2022</div>
+      <div>
+        <h2>cooking ingredients</h2>
+        <div>56,000₩</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
