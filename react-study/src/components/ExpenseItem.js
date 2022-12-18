1ca73f42af4eae2ad5025f56ac66a53885eca345
Dean <yjj052800@gmail.com>
2022-12-18 19:48:31 +0900

    print dynamic data

--- react-study/src/components/ExpenseItem.js
+++ react-study/src/components/ExpenseItem.js
@@ -1,12 +1,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 12, 18);
+  const expenseTitle = 'Cooking ingredients';
+  const expenseAmount = 56000;
   return (
     <div className="expense-item">
-      <div>December 18th 2022</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>cooking ingredients</h2>
-        <div className="expense-item__price">56,000₩</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}₩</div>
       </div>
     </div>
   );
