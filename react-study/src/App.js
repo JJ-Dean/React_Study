2549ee59787cf60d6047280293108118a77fb59e
Dean <yjj052800@gmail.com>
2022-12-18 23:33:22 +0900

    seperate ExpenseItem to card components

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,50 +1,31 @@
-import ExpenseItem from './components/ExpenseItem';
+import Expense from './components/Expense';
 
 function App() {
   const expenses = [
     {
       id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
+      title: '냄비 세트',
+      amount: 121000,
       date: new Date(2022, 5, 10),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 5, 12) },
+    { id: 'e2', title: '모니터', amount: 434000, date: new Date(2021, 5, 12) },
     {
       id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
+      title: '노트북 받침대',
+      amount: 10000,
       date: new Date(2022, 7, 28),
     },
     {
       id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
+      title: '책상 (나무)',
+      amount: 230000,
       date: new Date(2022, 12, 12),
     },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expense categories={expenses}></Expense>
     </div>
   );
 }
