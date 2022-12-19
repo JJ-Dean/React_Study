73be44306ad575613d4d293bb0db6ebc4341155e
Dean <yjj052800@gmail.com>
2022-12-20 01:41:17 +0900

    add submitHandler when click submit button to get input value

--- react-study/src/components/NewExpense/ExpenseForm.js
+++ react-study/src/components/NewExpense/ExpenseForm.js
@@ -3,44 +3,60 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const NewExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState('');
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enteredDate, setEnteredDate] = useState('');
-  const [userInput, setUserInput] = useState({
-    title: '',
-    amount: '',
-    date: '',
-  });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   title: '',
+  //   amount: '',
+  //   date: '',
+  // });
 
   const titleChangeHandler = (event) => {
-    //setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      title: event.target.value,
-    });
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   title: event.target.value,
+    // });
   };
 
   const amountChangeHandler = (event) => {
-    //setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      amount: event.target.value,
-    });
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   amount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
-    //setEnteredDate(event.target.value);
+    setEnteredDate(event.target.value);
+
+    // 전 State에 의존하는 State를 업데이트할 때 밑의 코드는 위험하다.
     // setUserInput({
     //   ...userInput,
     //   date: event.target.value,
     // });
-    setUserInput((prevState) => {
-      return { ...prevState, enteredTitle: event.target.value };
-    });
+
+    // 위에 코드보다 밑의 코드를 사용하자.
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value };
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
   };
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>물품 이름</label>
