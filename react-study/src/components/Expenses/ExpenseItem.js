3d970d99d1c57d0309894ed20f46e858dec320e5
Dean <yjj052800@gmail.com>
2022-12-19 20:26:48 +0900

    add button to change 'title' but it doesn't works

--- react-study/src/components/Expenses/ExpenseItem.js
+++ react-study/src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,11 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+  // document.getElementById('~~').addEventListner(); => 명령식 방식
+  const clickHandler = () => {
+    console.log('클릭했니');
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +15,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}₩</div>
       </div>
+      <button onClick={clickHandler}>품목 이름 바꾸기</button>
     </Card>
   );
 };
