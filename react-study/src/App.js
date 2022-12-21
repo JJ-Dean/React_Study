3717ff470641f8edc79dfb4e27bf3727b0c47d57
Dean <yjj052800@gmail.com>
2022-12-21 16:21:45 +0900

    use State store list to add new expense and show on UI

--- react-study/src/App.js
+++ react-study/src/App.js
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expense from './components/Expenses/Expense';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: '냄비 세트',
+    amount: 121000,
+    date: new Date(2019, 5, 10),
+  },
+  { id: 'e2', title: '모니터', amount: 434000, date: new Date(2020, 5, 12) },
+  {
+    id: 'e3',
+    title: '노트북 받침대',
+    amount: 10000,
+    date: new Date(2021, 7, 28),
+  },
+  {
+    id: 'e4',
+    title: '책상 (나무)',
+    amount: 230000,
+    date: new Date(2022, 12, 12),
+  },
+];
+
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: '냄비 세트',
-      amount: 121000,
-      date: new Date(2019, 5, 10),
-    },
-    { id: 'e2', title: '모니터', amount: 434000, date: new Date(2020, 5, 12) },
-    {
-      id: 'e3',
-      title: '노트북 받침대',
-      amount: 10000,
-      date: new Date(2021, 7, 28),
-    },
-    {
-      id: 'e4',
-      title: '책상 (나무)',
-      amount: 230000,
-      date: new Date(2022, 12, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = (expense) => {
-    console.log('App.js');
+    setExpenses((prevExpense) => {
+      return [expense, ...prevExpense];
+    });
     console.log(expense);
   };
 
