e96121a34b2c892948184472129c684c08c6fd5d
junjae <yjj052800@gmail.com>
2023-09-15 18:02:59 +0900

    장바구니 modal 추가

--- react-study07/src/components/Layout/Header.js
+++ react-study07/src/components/Layout/Header.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import { Fragment } from 'react';
 
 import mealsImg from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = () => {
+const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>음식 시키신 분</h1>
-        <HeaderCartButton></HeaderCartButton>
+        <HeaderCartButton onClick={props.onShowCart}></HeaderCartButton>
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImg} alt="A table full of delicious food!" />
