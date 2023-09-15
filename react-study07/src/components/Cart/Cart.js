00c1518883754cee31815eb11d69560a3d3047b8
Dean <yjj052800@gmail.com>
2023-09-15 23:34:22 +0900

    useContext로 모든 components 에서 접근 가능한 장바구니 데이터 생성 , useReducer로 state 관리

--- react-study07/src/components/Cart/Cart.js
+++ react-study07/src/components/Cart/Cart.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React from 'react';
 
 import classes from './Cart.module.css';
-import Modal from "../UI/Modal";
+import Modal from '../UI/Modal';
 
-const Cart = props => {
-    const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: '초밥', amount: 2, price: 12.99}].map(item => <li>{item.name}</li>)}</ul>;
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: '초밥', amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
 
-    return (
-        <Modal onClose={props.onClose}>
-            {cartItems}
-            <div className={classes.total}>
-                <span>총 수량</span>
-                <span>25.98</span>
-            </div>
-            <div className={classes.actions}>
-                <button className={classes['button--alt']} onClick={props.onCloseCart}>닫기</button>
-                <button className={classes.button}>주문하기</button>
-            </div>
-        </Modal>
-    )
-}
+  return (
+    <Modal onClose={props.onClose}>
+      {cartItems}
+      <div className={classes.total}>
+        <span>총 수량</span>
+        <span>25.98</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          닫기
+        </button>
+        <button className={classes.button}>주문하기</button>
+      </div>
+    </Modal>
+  );
+};
 
 export default Cart;
